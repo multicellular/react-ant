ee0658b483ffe6098c28068f0e25cfd484e4f918
justaworker <1438367681@qq.com>
2020-01-20 13:02:38 +0800

    元

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <Button type="primary"></Button>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +20,7 @@ const App: React.FC = () => {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
